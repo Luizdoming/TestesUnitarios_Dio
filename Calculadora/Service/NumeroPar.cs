ddb5b70bd478276818a253904631840409e31c70
Luiz domingo silva <luiz-elayny@live.com>
2023-12-14 13:54:05 -0300

    Criado os teste e validados

---
+++ Calculadora/Service/NumeroPar.cs
@@ -0,0 +1,8 @@
+
+public class NumerosPar
+{
+    public bool RetornarPar(int numero)
+    {
+        return numero % 2 == 0;
+    }
+}
--- CalculadoraTeste/CalculadoraTeste.cs
+++ CalculadoraTeste/CalculadoraTeste.cs
@@ -3,12 +3,14 @@ namespace CalculadoraTeste;
 public class CalculadoraTeste
 {
     private CalculadoraImpementada _resultadoSoma;
-
+    private NumerosPar _numeroPar;
     //Construtor da CalculadoraTest
     public CalculadoraTeste()
     {
+        _numeroPar = new NumerosPar();
         _resultadoSoma = new CalculadoraImpementada();
     }
+
 
     //Metodo onde ira realizar os testes da soma
     [Fact]
@@ -22,8 +24,28 @@ public class CalculadoraTeste
         // Fact
         int resultadoSomaCalculadora = _resultadoSoma.Somar(numeroOne, numerotwo);
 
-        // Assert -> temos que passar 2 parametros - 1° o resultado esperado , 2° o resultado da soma que fica na class onde estamos testando
+        // Assert -> temos que passar 2 parametros - 1° o resultado esperado , 
+        // 2° o resultado da soma que fica na class onde estamos testando
         Assert.Equal(resultadoEsperado, resultadoSomaCalculadora);
+    }
 
+    [Theory]
+    [InlineData(24)]
+    [InlineData(20)]
+    [InlineData(22)]
+    [InlineData(16)]
+    [InlineData(14)]
+    [InlineData(12)]
+    [InlineData(10)]
+    [InlineData(17)]
+    public void DeveRetornarSeOsNumerosEpar(int numero)
+    {
+        //Quando usamos a notação [Theory] -> não precisamos do Arrange
+
+        // Act
+        bool verificarNumerosPar = _numeroPar.RetornarPar(numero);
+
+        // Assert
+        Assert.True(verificarNumerosPar);
     }
 }
